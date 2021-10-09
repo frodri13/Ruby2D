38ac993b0f627c2643d673c93b686a16114a3176
Fabio <alessf13@gmail.com>
2021-10-09 13:03:14 +0200

    feat: shapes

---
+++ shapes.rb
@@ -0,0 +1,18 @@
+require 'ruby2d'
+
+# All attributes can be read and set individually
+
+# t = Triangle.new
+# t.x1 = 50
+# t.y3 = 25
+# t.color = 'red'
+
+Circle.new(
+  x: 200, y: 175,
+  radius: 150,
+  sectors: 32,
+  color: 'fuchsia',
+  z: 10
+)
+
+show
