37192d74a3385973cbc04368fb1b2374f8980b4f
Fabio <alessf13@gmail.com>
2021-10-09 13:27:12 +0200

    feat: hero animations

---
+++ hero.rb
@@ -0,0 +1,31 @@
+require 'ruby2d'
+
+hero = Sprite.new(
+	'./img/hero.png',
+	width: 78,
+	height: 99,
+	clip_width: 78, 
+	time: 250,
+	animations: {
+		walk: 1..2,
+		climb: 3..4,
+		cheer: 5..6
+	}
+)
+
+on :key_down do |event|
+	case event.key
+	when 'left'
+		hero.play animation: :walk, loop: true, flip: :horizontal
+	when 'right'
+		hero.play animation: :walk, loop: true
+	when 'up'
+		hero.play animation: :climb, loop: true
+	when 'down'
+		hero.play animation: :climb, loop: true, flip: :vertical
+	when 'c'
+		hero.play animation: :cheer
+	end
+end
+
+show
