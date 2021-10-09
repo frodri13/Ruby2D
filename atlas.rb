215dac1e9b27cf0168fd1acc9591c5053ee16820
Fabio <alessf13@gmail.com>
2021-10-09 13:40:36 +0200

    feat: atlas animation

---
+++ atlas.rb
@@ -0,0 +1,38 @@
+require 'ruby2d'
+
+atlas = Sprite.new(
+	'./img/atlas.png',
+	animations: {
+		count: [
+			{
+				x: 0, y: 0,
+				width: 35, height: 41,
+				time: 300
+			},
+			{
+				x: 26, y: 46, 
+				width: 35, height: 38,
+				time: 400
+			}, 
+			{
+				x: 65, y: 10, 
+				width: 32, height: 41,
+				time: 500
+			},
+			{
+				x: 10, y: 99, 
+				width: 32, height: 38,
+				time: 600
+			},
+			{
+				x: 74, y: 80, 
+				width: 32, height: 38,
+				time: 700
+			}
+		]
+	}
+)
+
+atlas.play animation: :count, loop: true
+
+show
